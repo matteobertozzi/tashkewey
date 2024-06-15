ce8a94951ebd523ebf0bbdd66d0d1b25ce4f91d0
Matteo Bertozzi <theo.bertozzi@gmail.com>
2024-06-15 12:41:31 +0200

    add demo build & run scripts

---
+++ scripts/build-and-run.sh
@@ -0,0 +1,30 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+./build.sh
+
+# Prepare dist bundle
+export TASHKEWEY_SERVICE_ROOT="build-dist"
+rm -rf ${TASHKEWEY_SERVICE_ROOT}
+mkdir -p ${TASHKEWEY_SERVICE_ROOT}
+cp -r tashkewey-netty/target/lib ${TASHKEWEY_SERVICE_ROOT}
+cp tashkewey-netty/target/tashkewey-netty-0.1.0-SNAPSHOT.jar ${TASHKEWEY_SERVICE_ROOT}/lib
+cp dummy-conf.json ${TASHKEWEY_SERVICE_ROOT}/config.json
+
+./scripts/run-service.sh
---
+++ scripts/run-service.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+set -e
+
+if [ -z "${TASHKEWEY_SERVICE_ROOT}" ]; then
+  echo "env variable TASHKEWEY_SERVICE_ROOT not set"
+  exit 1
+fi
+
+if [ -z "${JAVA_MAX_HEAP_SIZE}" ]; then
+  JAVA_MAX_HEAP_SIZE="1g"
+  JAVA_MAX_DMEM_SIZE="512m"
+fi
+
+# JDK Flags
+JDK_FLAGS="-Dfile.encoding=UTF8 -XX:+ShowCodeDetailsInExceptionMessages"
+JDK_FLAGS="${JDK_FLAGS} -XX:+UnlockExperimentalVMOptions"
+JDK_FLAGS="${JDK_FLAGS} -XX:+UseShenandoahGC -XX:ShenandoahMinFreeThreshold=40 -XX:ShenandoahInitFreeThreshold=20 -XX:ShenandoahUncommitDelay=10000 -XX:ShenandoahGuaranteedGCInterval=60000"
+#JDK_FLAGS="${JDK_FLAGS} -XX:+UseZGC -XX:+ZGenerational -XX:ZUncommitDelay=120"
+JDK_FLAGS="${JDK_FLAGS} -Xms16m -Xmx${JAVA_MAX_HEAP_SIZE} -XX:MaxDirectMemorySize=${JAVA_MAX_DMEM_SIZE}"
+#JDK_FLAGS="${JDK_FLAGS} -XX:+PrintFlagsFinal"
+
+# Netty Flags
+JDK_FLAGS="${JDK_FLAGS} -Dio.netty.maxDirectMemory=0"
+
+# Easer Insights Flags
+JDK_FLAGS="${JDK_FLAGS} -Deaser.insights.jdbc.pool.trace.connections=true -Deaser.insights.jdbc.pool.max.connections.per.thread=2"
+
+TASHKEWEY_CLASSPATH="${TASHKEWEY_SERVICE_ROOT}/modules/*:${TASHKEWEY_SERVICE_ROOT}/lib/*"
+if [ "${IS_AWS_SYS}" = "false" ]; then
+  ls ${TASHKEWEY_SERVICE_ROOT}/modules
+fi
+
+java ${JDK_FLAGS} -cp ${TASHKEWEY_CLASSPATH} io.github.matteobertozzi.tashkewey.Main -c ${TASHKEWEY_SERVICE_ROOT}/config.json
+TAS
