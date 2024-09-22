efc16809440cb3cb3bc9a5ab801f697aaf375a2c
Matteo Bertozzi <theo.bertozzi@gmail.com>
2024-09-22 15:44:43 +0200

    demo queues

--- demo-service/src/main/java/io/github/matteobertozzi/tashkewey/demo/DemoServiceHandlers.java
+++ demo-service/src/main/java/io/github/matteobertozzi/tashkewey/demo/DemoServiceHandlers.java
@@ -21,15 +21,20 @@ import java.nio.file.Path;
 import java.util.List;
 
 import io.github.matteobertozzi.easerinsights.logging.Logger;
+import io.github.matteobertozzi.rednaco.dispatcher.annotations.execution.AsyncQueue;
 import io.github.matteobertozzi.rednaco.dispatcher.annotations.execution.AsyncResult;
 import io.github.matteobertozzi.rednaco.dispatcher.annotations.execution.InlineFast;
 import io.github.matteobertozzi.rednaco.dispatcher.annotations.message.QueryParam;
 import io.github.matteobertozzi.rednaco.dispatcher.annotations.session.AllowPublicAccess;
+import io.github.matteobertozzi.rednaco.dispatcher.annotations.session.RateLimited;
+import io.github.matteobertozzi.rednaco.dispatcher.annotations.session.RateLimited.RateLimitOn;
+import io.github.matteobertozzi.rednaco.dispatcher.annotations.session.TokenSession;
 import io.github.matteobertozzi.rednaco.dispatcher.annotations.uri.UriMapping;
 import io.github.matteobertozzi.rednaco.dispatcher.annotations.uri.UriPrefix;
 import io.github.matteobertozzi.rednaco.dispatcher.message.MessageError;
 import io.github.matteobertozzi.rednaco.dispatcher.message.MessageException;
 import io.github.matteobertozzi.rednaco.dispatcher.message.MessageHandler;
+import io.github.matteobertozzi.rednaco.dispatcher.session.AuthSession;
 import io.github.matteobertozzi.rednaco.strings.HumansUtil;
 import io.github.matteobertozzi.rednaco.threading.ThreadUtil;
 
@@ -49,7 +54,7 @@ public class DemoServiceHandlers implements MessageHandler {
   @UriMapping(uri = "/standard")
   public List<DemoObject> standard(@QueryParam("ms") final long ms) {
     Logger.debug("hello standard {}", HumansUtil.humanTimeMillis(ms));
-    //ThreadUtil.sleep(ms);
+    ThreadUtil.sleep(ms);
     return List.of(new DemoObject(1, "hello"), new DemoObject(2, "standard"));
   }
 
@@ -79,5 +84,46 @@ public class DemoServiceHandlers implements MessageHandler {
   @UriMapping(uri = "/path")
   public Path patho() {
     return Path.of("./hello.txt");
+  }
+
+  @AllowPublicAccess
+  @UriMapping(uri = "/rate-limited/ip")
+  @RateLimited(on = RateLimitOn.IP, limit = 1)
+  public void rateLimitedOnIp() {
+    Logger.debug("Rate Limited call");
+  }
+
+  @AllowPublicAccess
+  @UriMapping(uri = "/rate-limited/session")
+  @RateLimited(on = RateLimitOn.SESSION_OWNER, limit = 1)
+  public void rateLimitedOnSessionOwner(@TokenSession final AuthSession session) {
+    Logger.debug("Rate Limited call on session owner: {}", session);
+  }
+
+  @AllowPublicAccess
+  @UriMapping(uri = "/queue/single")
+  @AsyncQueue(id = "single.queue")
+  public void singleQueue() {
+    Logger.debug("single queue (START)");
+    ThreadUtil.sleep(3500);
+    Logger.debug("single queue (END)");
+  }
+
+  @AllowPublicAccess
+  @UriMapping(uri = "/queue/concurrent")
+  @AsyncQueue(id = "concurrent.queue", concurrency = 2)
+  public void concurrentQueue() {
+    Logger.debug("concurrent 2thrd queue (START)");
+    ThreadUtil.sleep(3500);
+    Logger.debug("concurrent 2thrd queue (END)");
+  }
+
+  @AllowPublicAccess
+  @UriMapping(uri = "/queue/session")
+  @AsyncQueue(id = AsyncQueue.SESSION_OWNER_QUEUE, concurrency = 2)
+  public void sessionOwnerQueue(@TokenSession final AuthSession session) {
+    Logger.debug("single owner queue (START): {}", session);
+    ThreadUtil.sleep(3500);
+    Logger.debug("single owner queue (END): {}", session);
   }
 }
